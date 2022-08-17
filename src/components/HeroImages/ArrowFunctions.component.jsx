6e1bfd4be09fd5234ef77fb552d8d460aee168d4
radhikarama <74363007+radhikarama@users.noreply.github.com>
2022-08-17 14:21:19 +0530

    chages

--- src/components/HeroImages/ArrowFunctions.component.jsx
+++ src/components/HeroImages/ArrowFunctions.component.jsx
@@ -4,14 +4,21 @@ import React from 'react'
 export const NextArrow = (props) => {
   return (
    <>
-    <div {...props}/>
+    <div 
+    className={props.className}
+    style={{...props.style,backgroundColor:'#eb2f41' }}
+    onClick={props.onClick}
+    />
     </>
   )
 }
+// {...props}
 export const PrevArrow = (props) => {
   return (
    <>
-    <div {...props} />
+    <div  className={props.className}
+    style={{...props.style,backgroundColor:'#eb2f41' }}
+    onClick={props.onClick} />
     </>
   )
 }
